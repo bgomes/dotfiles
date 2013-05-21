59bf22ac6786b64574704471dff7824284750ea0
Hersonls <hersonls@gmail.com>
2013-05-21 16:28:42 -0300

    Changed python alias location and added a awesome ascii logo

--- sync.sh
+++ sync.sh
@@ -2,14 +2,14 @@
 
 cd "$(dirname "${BASH_SOURCE}")"
 
-git pull
+# git pull
 
-function doIt() {
+function sync_dot_files() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "sync.sh"  --exclude "install_deps.sh" --exclude "README.md" -av --no-perms . ~
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
+	sync_dot_files
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo
@@ -18,5 +18,5 @@ else
 	fi
 fi
 
-unset doIt
+unset sync_dot_files
 source ~/.zshrc
